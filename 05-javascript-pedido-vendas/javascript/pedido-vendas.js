822dc1695f36a31c339c446131ec34ebb9184a13
João Carlos <joaoslz@gmail.com>
2018-11-27 21:02:07 -0300

    aula javascript 27-11-2018

--- 05-javascript-pedido-vendas/javascript/pedido-vendas.js
+++ 05-javascript-pedido-vendas/javascript/pedido-vendas.js
@@ -5,24 +5,59 @@ let inputs = [
     document.querySelector('#valor').value 
 ];
 
+let tBody = document.querySelector('#tbodyItemPedido');
 
 btnCadastro.addEventListener('click',  function (event ) {
     event.preventDefault();
-    console.log(inputs );
-
-    inputs.forEach(input => console.log(input) );
-
+    criaLinhaItemItemPedido();
 });
 
+function criaLinhaItemItemPedido() {
+    let trItemPedido = document.createElement('tr');
+    
+    inputs.forEach(campo => criaCelulaTabela(trItemPedido, campo )  );
+    criaCelulaSubTotal(trItemPedido );
+   
+    adicionaNovoItemNaTabelaPedidoVendas(trItemPedido );
+}
+
+function adicionaNovoItemNaTabelaPedidoVendas(trItemPedido ) {
+    tBody.appendChild( trItemPedido );
+}
+
+
+function criaTd() {
+    return document.createElement('td');
+
+}
+
+function criaCelulaTabela(trItemPedido, input ) {
+    let td =  criaTd();
+    td.textContent = input;
+
+    trItemPedido.appendChild(td);
+}
+
+function criaCelulaSubTotal(trItemPedido ) {
+   let subTotal =  calculaSubTotal(  inputs[1], inputs[2] );
+   
+   let tdSubTotal = criaTd();
+   tdSubTotal.textContent = subTotal;
+   trItemPedido.appendChild(tdSubTotal );
+}
+
+function calculaSubTotal(quantidade, valor ) {
+    return quantidade * valor;
+}
 
 
 
 /*
-let btnCadastro = document.querySelector('#btnCadastro');
+let btnCadastrItemPedidoo = document.querySelector('#btnCadastrItemPedidoo');
 
-btnCadastro.addEventListener('click', cadastraProduto  );
+btnCadastrItemPedidoo.addEventListener('click', cadastrItemPedidoaProduto  );
 
-function cadastraProduto(event ) {
+function cadastrItemPedidoaProduto(event ) {
     event.preventDefault();
     console.log(inputs );
 }
